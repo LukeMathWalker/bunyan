e88c3882562f856bc00629bdcbbd7b64781261b5
Luca Palmieri <lpalmieri@truelayer.com>
2020-10-16 21:39:58 +0100

    Color test. Red.

--- tests/all/formatting.rs
+++ tests/all/formatting.rs
@@ -11,3 +11,14 @@ fn simple_log() {
         "[2012-02-08T22:56:52.856Z]  INFO: myservice/123 on example.com: My message\n",
     ));
 }
+
+#[test]
+fn simple_log_with_color() {
+    let input_path = get_corpus_path().join("simple.log");
+
+    let mut cmd = command();
+    cmd.arg("--color").pipe_stdin(input_path).unwrap();
+    cmd.assert().success().stdout(predicate::str::similar(
+        "[2012-02-08T22:56:52.856Z] \u{001b}[36m INFO\u{001b}[39m: myservice/123 on example.com: \u{001b}[36mMy message\u{001b}[39m\u{001b}[0m",
+    ));
+}
