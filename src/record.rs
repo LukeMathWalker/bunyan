cc86bee7bca687631bc0e98e1c714a74db8c4f9a
Luca Palmieri <lpalmieri@truelayer.com>
2020-10-16 18:41:10 +0100

    Make field pub.

--- src/record.rs
+++ src/record.rs
@@ -7,22 +7,22 @@ pub struct LogRecord<'a> {
     /// Thereafter, starting with 1, this will be incremented if there is any backward incompatible
     /// change to the log record format.
     #[serde(rename = "v")]
-    version: u8,
+    pub version: u8,
     /// See `LogLevel`
-    level: u8,
+    pub level: u8,
     /// Name of the service/application emitting logs in bunyan format.
-    name: &'a str,
+    pub name: &'a str,
     /// Name of the operating system host.
-    hostname: &'a str,
+    pub hostname: &'a str,
     /// Process identifier.
     #[serde(rename = "pid")]
-    process_identifier: u32,
+    pub process_identifier: u32,
     /// The time of the event captured by the log in [ISO 8601 extended format](http://en.wikipedia.org/wiki/ISO_8601).
-    time: DateTime<Utc>,
+    pub time: DateTime<Utc>,
     /// Log message.
     #[serde(rename = "msg")]
-    message: &'a str,
+    pub message: &'a str,
     /// Any extra contextual piece of information in the log record.
     #[serde(flatten)]
-    extras: serde_json::Value,
+    pub extras: serde_json::Value,
 }
--- src/sources/stdin.rs
+++ src/sources/stdin.rs
@@ -5,10 +5,12 @@ pub fn process_stdin() {
     let stdin = std::io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
-        println!("{}", line);
         match serde_json::from_str::<LogRecord>(&line) {
-            Ok(_) => println!("Parsed!"),
-            Err(e) => println!("Failed to parse - {:?}", e),
+            Ok(r) => println!(
+                "[{}] {}: {}/{} on {}: {}",
+                r.time, r.level, r.name, r.process_identifier, r.hostname, r.message
+            ),
+            Err(e) => eprintln!("Failed to parse - {:?}", e),
         }
     }
 }
