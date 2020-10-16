fc749eb53c49be714ee8805e986ae1b613d984e2
Luca Palmieri <lpalmieri@truelayer.com>
2020-10-16 19:06:47 +0100

    Remove gzip file. Travers all crashers to make sure the CLI does not crash.

--- tests/all/crashers.rs
+++ tests/all/crashers.rs
@@ -2,9 +2,12 @@ use crate::helpers::{command, get_corpus_path};
 
 #[test]
 fn does_not_crash() {
-    let crasher_path = get_corpus_path().join("old-crashers").join("139.log");
+    let crashers_dir = std::fs::read_dir(get_corpus_path().join("old-crashers")).unwrap();
+    for crasher in crashers_dir {
+        let crasher = crasher.unwrap();
 
-    let mut cmd = command();
-    cmd.pipe_stdin(crasher_path).unwrap();
-    cmd.assert().success();
+        let mut cmd = command();
+        cmd.pipe_stdin(crasher.path()).unwrap();
+        cmd.assert().success();
+    }
 }
