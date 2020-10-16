d4a881402399ffedd392ee20f09473c515be6b44
Luca Palmieri <lpalmieri@truelayer.com>
2020-10-16 23:24:47 +0100

    Extra log with color.

--- tests/all/formatting.rs
+++ tests/all/formatting.rs
@@ -33,3 +33,14 @@ fn extra_field_log() {
         "[2012-02-08T22:56:52.856Z]  INFO: myservice/123 on example.com: My message (extra=field)\n",
     ));
 }
+
+#[test]
+fn extra_field_log_with_color() {
+    let input_path = get_corpus_path().join("extrafield.log");
+
+    let mut cmd = command();
+    cmd.arg("--color").pipe_stdin(input_path).unwrap();
+    cmd.assert().success().stdout(predicate::str::similar(
+        "[2012-02-08T22:56:52.856Z]  \u{1b}[36mINFO\u{1b}[0m: myservice/123 on example.com: \u{1b}[36mMy message\u{1b}[0m (extra=field)\n",
+    ));
+}
