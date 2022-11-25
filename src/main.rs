c09111511ccd4a4a6de50bccbfd20133a8588929
Marco Grassi <marco.gra@gmail.com>
2022-11-25 12:26:34 +0800

    upgrade to clap 4

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,31 @@ use clap::Parser;
 /// Bunyan is a simple and fast JSON logging library for node.js services,
 /// a one-JSON-object-per-line log format, and a bunyan CLI tool for nicely viewing those logs.
 /// this is a Rust implementation of bunyan cli used to filter and pretty-print Bunyan log file content.
-#[derive(clap::StructOpt)]
-#[clap(version = "0.1", author = "Luca Palmieri <rust@lpalmieri.com>")]
+#[derive(Parser)]
+#[command(version = "0.1", author = "Luca Palmieri <rust@lpalmieri.com>")]
 struct Cli {
     /// Only show messages at or above the specified level.
     ///
     /// You can specify level names (trace, debug, info, warn, error, fatal) or a positive
     /// numeric value.
-    #[clap(short, long, default_value = "trace")]
+    #[arg(short, long, default_value = "trace")]
     level: NumericalLogLevel,
     /// Specify an output format.
     ///
     /// - long: prettified JSON;
-    #[clap(short, long, default_value = "long")]
+    #[arg(short, long, default_value = "long")]
     output: Format,
     /// Colorize output.
     ///
     /// Defaults to try if output stream is a TTY.
-    #[clap(long = "color", conflicts_with = "no-color")]
+    #[arg(long = "color", conflicts_with = "no-color")]
     color: bool,
     /// Force no coloring (e.g. terminal doesn't support it).
-    #[clap(long = "no-color", conflicts_with = "color")]
+    #[arg(name = "no-color", long = "no-color", conflicts_with = "color")]
     no_color: bool,
     /// Suppress all but legal Bunyan JSON log lines. By default non-JSON and non-Bunyan lines
     /// are passed through.
-    #[clap(long)]
+    #[arg(long)]
     strict: bool,
 }
 
